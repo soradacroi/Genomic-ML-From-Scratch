6382a55b7b5c3443709711198d8829c1343dce35
soradacroi <soradacroi@gmail.com>
2026-01-28 02:13:27 +0530

    model???

--- src/counting/counting.py
+++ src/counting/counting.py
@@ -1,40 +1,42 @@
 import csv
 import random as r
+from decimal import Decimal, getcontext
+getcontext().prec = 5
 
 files = {'data/human.csv': 'human', 'data/dog.csv': 'dog'}
-train_data = []
+training_data = []
 test_data = []
 for f, species in files.items():
     with open(f, 'r') as csvfile:
         reader = csv.DictReader(csvfile)
         all_rows = list(reader) 
         r.shuffle(all_rows)
-        train_data += [((row['sequence']), species) for row in all_rows[:810]]
+        training_data += [((row['sequence']), species) for row in all_rows[:810]]
         test_data += [((row['sequence']), species) for row in all_rows[810:820]]
 
 
 G = {"A", "G", "C", "T", "N"}
 n = 3
 
-def make_str(n=3, G={"A", "G", "C", "T", "N"}):
+def make_str(n=n, G={"A", "G", "C", "T", "N"}):
     if n == 1:
         return G
     output = []
     combos = make_str(n-1, G)
     for item in combos:
         for char in G:
-            output.append(item + char)
-            
+            output.append(item + char)  
     return output
 
 d_h = {}
 d_d = {}
 
-for i in make_str(3):
+for i in make_str():
     d_h[i], d_d[i] = 0, 0
 
+print(len(d_h))
 
-def split_g(g, n = 3):
+def split_g(g, n = n):
     i = len(g)
     j = 0
     m = []
@@ -44,21 +46,29 @@ def split_g(g, n = 3):
     return m
     
             
-
-for i in train_data:
+h, d = 0, 0
+for i in training_data:
     s_g = ((split_g(i[0])))
+    
     if i[1] == "human":
         for i in s_g:
             d_h[i] += 1
+            h += 1
     elif i[1] == "dog":
         for i in s_g:
-            d_d[i] += 1   
+            d_d[i] += 1  
+            d += 1 
 
-print(d_h)
-print()
-print(d_d)
-        
+
+d_x_h = {}
+d_x_d = {}
+for i in d_h:
+    d_x_h[i] = (d_h[i])/(h)
+for i in d_h:
+    d_x_d[i] = (d_d[i])/(d) 
+
+print(d_x_h)
+print(d_x_d)
 
 
     
-
